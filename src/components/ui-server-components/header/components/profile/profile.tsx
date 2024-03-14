0172282cdc593ccda43a67182091580d3fcfa56c
Alper Erdogan <alpererdogan8@gmail.com>
2024-03-14 15:35:33 +0300

    feat(style): added margin height to logo card

--- src/components/ui-server-components/header/components/profile/profile.tsx
+++ src/components/ui-server-components/header/components/profile/profile.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Profile() {
   return (
-    <Link href="/" className="flex flex-col">
+    <Link href="/" className="flex mt-5  flex-col">
       <p className="font-bold text-xl drop-shadow-sm hover:drop-shadow-md">Æ{/* Alper Erdoğan */}</p>
       <TooltipProvider>
         <Tooltip>
           <TooltipTrigger>
-            <div className="flex gap-2 items-center justify-between">
-              <p className=" font-normal text-[16.5px] drop-shadow-sm">software developer</p>
+            <div className="  flex gap-2 items-center justify-between">
+              <p className=" font-normal text-[16.5px] whitespace-nowrap drop-shadow-sm">software developer</p>
 
               <div className="flex items-center justify-center">
                 <div className="w-2 h-2 rounded-full bg-lime-400"></div>
