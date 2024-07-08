86c0309c49fb9bd6d7f3533091624f047902ac8a
Alper Erdogan <alpererdogan8@gmail.com>
2024-07-08 16:50:13 +0300

    feat: Integrate theme cookie generation in ThemeProvider

--- src/hooks/theme-provider.tsx
+++ src/hooks/theme-provider.tsx
@@ -1,9 +1,28 @@
 'use client';
 
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ThemeProvider as NextThemesProvider, useTheme } from 'next-themes';
 import { type ThemeProviderProps } from 'next-themes/dist/types';
+import { useEffect } from 'react';
+
+function ManifestThemeGenerateCookie({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { theme, resolvedTheme } = useTheme();
+
+  useEffect(() => {
+    const actualTheme = resolvedTheme || theme;
+    document.cookie = `theme=${actualTheme};path=/;max-age=31536000`;
+  }, [theme, resolvedTheme]);
+
+  return children;
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <ManifestThemeGenerateCookie>{children}</ManifestThemeGenerateCookie>
+    </NextThemesProvider>
+  );
 }
