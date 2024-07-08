3cd6ddb7e8c1c89d79bde672f94e7cc9e4c342da
Alper Erdogan <alpererdogan8@gmail.com>
2024-07-08 16:51:28 +0300

    style: changed profile style

--- src/components/ui-server-components/header/components/profile/profile.tsx
+++ src/components/ui-server-components/header/components/profile/profile.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Tooltip,
   TooltipContent,
@@ -5,34 +6,39 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import Link from 'next/link';
-
 export default function Profile() {
   return (
-    <div>
-      <Link href="/" className="flex mt-5  flex-col">
-        <p className="font-bold text-xl drop-shadow-sm hover:drop-shadow-md">
-          Æ{/* Alper Erdoğan */}
-        </p>
-      </Link>
-      <TooltipProvider>
-        <Tooltip delayDuration={100}>
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <div className="px-2 py-1.5 rounded-lg hover:bg-accent">
           <TooltipTrigger>
-            <div className="  flex gap-2 items-center justify-between">
-              <p className=" font-normal text-[16.5px] whitespace-nowrap drop-shadow-sm">
-                software developer
-              </p>
-
-              <div className="flex items-center justify-center">
+            <Link href="/" className="flex justify-between ">
+              <p className="font-bold text-xl drop-shadow-sm hover:drop-shadow-md">
+                Æ{/* Alper Erdoğan */}
+              </p>{' '}
+              <div className="flex items-center justify-center mr-0.5">
                 <div className="w-2 h-2 rounded-full bg-lime-400"></div>
                 <div className="w-2 h-2  rounded-full bg-lime-200 absolute -z-10 blur-[4px]"></div>
               </div>
+            </Link>
+
+            <div className="flex gap-2 items-center justify-between">
+              <p className="font-normal text-[16.5px] whitespace-nowrap drop-shadow-sm">
+                software developer
+              </p>
             </div>
           </TooltipTrigger>
-          <TooltipContent side="bottom" align="end">
-            <p className="font-mono text-[.9rem] ">Open to work</p>
+          <TooltipContent
+            className="border-2 shadow-none"
+            side="bottom"
+            alignOffset={-10}
+            sideOffset={12}
+            align="end"
+          >
+            <p className="font-semibold text-[.9rem] ">Open to work</p>
           </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-    </div>
+        </div>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
